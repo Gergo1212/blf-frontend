4a9f7dfe4807c9ca2c65c4d4294c7cbc52071c8d
gergo1212 <gergo.meszaros.codecool@gmail.com>
2021-02-03 19:06:32 +0100

    imported Calendar to TeamAdmin.js

--- src/page/admin/TeamAdmin.js
+++ src/page/admin/TeamAdmin.js
@@ -1,13 +1,16 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {UtilContext} from "../../context/UtilContext";
 import {useParams} from "react-router";
 import AdminNavbar from "../../component/AdminNavbar";
 import TableCreator from "../../component/TableCreator";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 
 function TeamAdmin() {
 
     const {requestGet, elements} = useContext(UtilContext);
     const {id} = useParams();
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         requestGet(`http://localhost:8091/team/${id}`)
@@ -16,7 +19,9 @@ function TeamAdmin() {
     function listTeamMembers(team) {
         if (team.teamMember !== undefined) {
             return elements.teamMember.map((member) => (
+                member.Aktív === 1 ?
                 <li>{member.id} | {member.name}</li>
+                    : <li style={{color:"red"}}>{member.id} | {member.name}</li>
             ))
         } else {
             return null;
@@ -26,6 +31,7 @@ function TeamAdmin() {
     return (
         <React.Fragment>
             <AdminNavbar/>
+            <Calendar onChange={setDate} value={date}/>
             <TableCreator inputObjects={[elements]}/>
             <div style={{color: "white"}}>{listTeamMembers(elements)}</div>
         </React.Fragment>
